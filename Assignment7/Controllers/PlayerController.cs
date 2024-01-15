8ddbde6c18cee4c734a24b72a6e4c495b2c68e6f
Rohithkumar <rohithkumargande@gmail.com>
2024-01-15 15:16:36 +0530

    Azure

---
+++ Assignment7/Controllers/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using Assignment7.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment7.Controllers
+{
+    public class PlayerController : Controller
+    {
+        static List<Player> PlayerList = new List<Player>()
+        {
+           new Player() {PId=1,PName="Rohith",PCountry="India",PType="All-rounder"},
+           new Player() {PId=2,PName="V.Kohili",PCountry="India",PType="Batsman"},
+           new Player() {PId=3,PName="David",PCountry="Australia",PType="Wicketkeeper"}
+        };
+        public IActionResult Index()
+        {
+            return View(PlayerList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Player());
+        }
+        [HttpPost]
+        public IActionResult Create(Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                PlayerList.Add(player);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
